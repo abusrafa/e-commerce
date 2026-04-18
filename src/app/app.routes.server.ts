feb9b2103f7400545bb41958dedf9b8f8f0429cf
abusrafa <abusrafa@gmail.com>
2026-04-19 01:45:15 +0200

    Fix: specific render modes for dynamic routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,36 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'productDetails/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'Products/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'Products/brand/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'Products/category/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'Products/subcategory/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'categories/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Server,
+  },
+
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
